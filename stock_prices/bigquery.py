58dc4f7f016698bf3c646924a5cfc1c6b3be520e
spower136 <s.power136@yahoo.com>
2022-05-08 12:37:44 -0700

    adding dockerfile to stock prices dir

--- stock_prices/bigquery.py
+++ stock_prices/bigquery.py
@@ -1,19 +1,25 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
+from utils import get_client
 
-key_path = "msds_434.json"
+# key_path = "msds_434.json"
 
-credentials = service_account.Credentials.from_service_account_file(key_path)
-project = 'msds-434-347202'
-client = bigquery.Client(project=project, credentials=credentials)
+# credentials = service_account.Credentials.from_service_account_file(key_path)
+# project = 'msds-434-347202'
+# client = bigquery.Client(project=project, credentials=credentials)
 
+
+client = get_client()
 
 query = """
     SELECT *
     FROM stock_prices.stock_details
 """
 
-query_job = client.query(query) # make an API request
+def query_bq():
+    query_job = client.query(query) # make an API request
+    df = query_job.to_dataframe()
+    return df
+    # print(df.head())
 
-df = query_job.to_dataframe()
-print(df.head())
+query_bq()
