3c22379988d04c6c0ad3ecbd1117bc87598a790e
spower136 <s.power136@yahoo.com>
2022-05-09 10:45:21 -0700

    cleanup

--- stock_prices/bigquery.py
+++ stock_prices/bigquery.py
@@ -2,13 +2,6 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 from utils import get_client
 
-# key_path = "msds_434.json"
-
-# credentials = service_account.Credentials.from_service_account_file(key_path)
-# project = 'msds-434-347202'
-# client = bigquery.Client(project=project, credentials=credentials)
-
-
 client = get_client()
 
 query = """
